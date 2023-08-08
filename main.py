e2e65c7c753294244ed49ad4da6cf3eb3d940b2c
Jonathan Licari <jon@Jonathans-MacBook-Air.local>
2023-08-08 07:45:37 -0400

    initial setup for pdf scraper

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from camelot import read_pdf
+
+
+def scrape_dollar_amounts(pdf_path):
+    tables = read_pdf(pdf_path, flavor="stream")
+    dollar_amounts = []
+
+    for table in tables:
+        for row in table.df.itertuples(index=False):
+            for cell in row:
+                if isinstance(cell, str) and "$" in cell:
+                    dollar_amounts.append(cell)
+
+    return dollar_amounts
+
+
+if __name__ == "__main__":
+    # Get the path to the PDF file
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    pdf_path = os.path.join(script_dir, "data", "report.pdf")
+
+    # Scrape dollar amounts from the PDF
+    dollar_amounts = scrape_dollar_amounts(pdf_path)
+
+    # Print the scraped dollar amounts
+    print("Scraped Dollar Amounts:")
+    for amount in dollar_amounts:
+        print(amount)
+
