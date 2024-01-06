903d475cf12c0b2b91da2376c1d288dfed6ce9c0
jonLicari <jlicari@ryerson.ca>
2024-01-05 22:01:36 -0500

    clean up filtering and printing to terminal

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import os
 import pandas as pd
 from camelot import read_pdf
 
+columns = ['Date', 'Name', 'Amount']
+
+
+class FilteredTable(pd.DataFrame):
+    '''Table for holding data filtered from the parsed pdf.'''
+    def __init__(self, columns=None):
+        super().__init__(columns=columns)
+
+    def assign_row(self, row: list, index: int):
+        '''Assign a new row to the dataframe at the specified index.'''
+        super().loc[index] = row
+
 
 def parse_data_from_input_file(pdf_path) -> list:
     '''Read the input file and parse data into tables.'''
     table_list = read_pdf(pdf_path, pages="2,3", flavor="stream")
-    # dollar_amounts = []
-
-    # for table in tables:
-    #     for row in table.df.itertuples(index=False):
-    #         for cell in row:
-    #             if isinstance(cell, str) and "$" in cell:
-    #                 dollar_amounts.append(cell)
 
     return table_list
 
@@ -31,34 +36,32 @@ def get_input_file_path() -> str:
 
 
 def filter_tables(table) -> pd.DataFrame:
-    '''Filter table data and save to a new df.'''
-    new_df = pd.DataFrame()
+    '''Filter table data row-by-row and save to a new df.'''
+    # New dataframe to read in filtered data to
+    new_df = FilteredTable(columns=columns)
 
+    # Iterate over the raw data table row-by-row and filter data
     for row in table.df.itertuples(index=False):
-        if len(row) < 4:
+        filtered_row = []
+
+        # save transaction date, ignore post date
+        try:
+            filtered_row.append(
+                pd.to_datetime(row[0]+" 2023", format='%b %d %Y'))
+
+        except Exception:
             continue
-        if row[4] != '':
-            filtered_row = []
 
-            # save transaction date, ignore post date
-            try:
-                filtered_row.append(
-                    pd.to_datetime(row[0]+" 2023", format='%b %d %Y'))
+        filtered_row.append(row[2])
 
-            except Exception as error:
-                print(row[0], "failed", error)
-                continue
+        try:
+            filtered_row.append((row[4]))
+        except Exception as error:
+            print(row[4], "failed", error)
+            continue
 
-            filtered_row.append(row[2])
-
-            try:
-                filtered_row.append(int(row[4]))
-            except Exception as error:
-                print(row[4], "failed>>>", error)
-                continue
-
-            print(filtered_row)
-            new_df.loc[len(new_df)] = filtered_row
+        # print(filtered_row)
+        new_df.assign_row(row=filtered_row, index=len(new_df))
 
     return new_df
 
@@ -66,8 +69,6 @@ def filter_tables(table) -> pd.DataFrame:
 def print_to_terminal(table: pd.DataFrame) -> None:
     '''Print filtered table contents to the terminal.'''
     print(table)
-    for row in table.itertuples(index=False):
-        print(row)
 
 
 if __name__ == "__main__":
@@ -84,8 +85,7 @@ if __name__ == "__main__":
     for n in range(table_count):
         print("TABLE", n)
         print("Size = ", tables[n].shape)
-        # filter_tables(tables[n])
-    filtered_table: pd.DataFrame = filter_tables(tables[0])
+        filtered_table: pd.DataFrame = filter_tables(tables[n])
 
-    # Print to terminal
-    print_to_terminal(filtered_table)
+        # Print to terminal
+        print_to_terminal(filtered_table)
